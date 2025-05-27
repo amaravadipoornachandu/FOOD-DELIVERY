d085b25a4776f6d6533afa2e1b48434751992aed
Poorna2IT <164735214+Poorna2IT@users.noreply.github.com>
2025-05-27 17:46:12 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 const Add = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-pary.onrender.com";
   const [image, setImage] = React.useState(false);
   const [data, setData] = React.useState({
     name: "",
